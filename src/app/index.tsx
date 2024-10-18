964b1e66d2294213b9982f8e4ab849f761a9d70f
Eduardo da Silva <avlis.edu@hotmail.com>
2024-10-18 11:45:09 -0300

    api

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,6 +2,11 @@ import { Text, View, ScrollView } from "react-native";
 import { Header } from "../components/header";
 import Constants from 'expo-constants'
 import { Banner } from "../components/banner";
+import { Search } from "../components/search"
+import { Section } from "../components/section"
+import { TrendingFoods } from "../components/trending";
+
+
 const statusBarHeight = Constants.statusBarHeight
 
 
@@ -15,7 +20,20 @@ export default function Index() {
       <View className="w-full px-4" style={{ marginTop: statusBarHeight + 8}}>
         <Header/>
         <Banner/>
+
+        <Search/>
       </View>
+
+      <Section
+        name="Comidas em alta"
+        label="Veja mais"
+        action={ () => console.log("Clicou em veja mais")}
+        size="text-2xl"
+      />
+
+      <TrendingFoods/>
+
+       
     </ScrollView>
      
   );
--- src/components/banner/index.tsx
+++ src/components/banner/index.tsx
@@ -3,27 +3,27 @@ import PagerView from "react-native-pager-view"
 
 export function Banner() {
  return (
-   <View className='w-full h-36 rounded-2x1 mt-5 mb-4'>
+   <View className='w-full h-36 md: h-60 rounded-2x1 mt-5 mb-4'>
     <PagerView style={{flex: 1}} initialPage={0} pageMargin={14}>
         <Pressable  
-        className='w-full h-36 rounded-2x1'
+        className='w-full h-36 md: h-60 rounded-2x1' //md faz o ajuste automatico
         key="1"
         onPress={() => console.log("Clicou no banner 1")}
         >
             <Image
             source={require("../../assets/banner1.png")}
-            className='w-full h-36 rounded-2xl'
+            className='w-full h-36 md: h-60 rounded-2xl'
             />
         </Pressable>
 
         <Pressable  
-        className='w-full h-36 rounded-2x1'
+        className='w-full h-36 md: h-60 rounded-2x1'
         key="2"
         onPress={() => console.log("Clicou no banner 2")}
         >
             <Image
             source={require("../../assets/banner2.png")}
-            className='w-full h-36 rounded-2xl'
+            className='w-full h-36 md: h-60 rounded-2xl'
             />
         </Pressable>
 
